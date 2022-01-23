3f7157b4fa696efca937f8604df7b22c57f4cc90
Mike Prybytkin <prybytkin@bk.ru>
2022-01-23 23:09:03 +0300

    feat: burger-menu part 1

--- portfolio/scripts/index.js
+++ portfolio/scripts/index.js
@@ -1,4 +1,14 @@
-document.querySelector('.burger-menu').addEventListener('click', function (e) {
-    e.preventDefault();
-    this.classList.toggle('is-active');
-})
+// document.querySelector('.burger-menu').addEventListener('click', function (e) {
+//     e.preventDefault();
+//     this.classList.toggle('is-active');
+// })
+
+
+const burgerMenu = document.querySelector('.burger-menu');
+const navList = document.querySelector('.nav-list');
+
+function toggleMenu() {
+    burgerMenu.classList.toggle('is-active');
+    navList.classList.toggle('is-active');
+}
+burgerMenu.addEventListener('click', toggleMenu);
