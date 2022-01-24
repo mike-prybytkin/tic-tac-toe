2489500e34c879c52ad1e7e406f0a5c21b90b1a1
Mike Prybytkin <prybytkin@bk.ru>
2022-01-24 23:06:05 +0300

    feat: complete burger-menu

--- portfolio/scripts/index.js
+++ portfolio/scripts/index.js
@@ -1,12 +1,5 @@
-// document.querySelector('.burger-menu').addEventListener('click', function (e) {
-//     e.preventDefault();
-//     this.classList.toggle('is-active');
-// })
-
-
 const burgerMenu = document.querySelector('.burger-menu');
 const navList = document.querySelector('.nav-list');
-
 
 function toggleMenu() {
     burgerMenu.classList.toggle('is-active');
@@ -17,14 +10,14 @@ burgerMenu.addEventListener('click', toggleMenu);
 
 
 
-// const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach((el) => el.addEventListener('click', closeMenu));
 
+function closeMenu(event) {
+    if (event.target.classList.contains('nav-link')) {
+    burgerMenu.classList.remove('is-active');
+    navList.classList.remove('is-active');
+    }
+  }
 
-// function closeMenu(event) {
-//     if (event.target.classList.contains('nav-link')) {
-//       document.querySelector('.burger-menu').classList.remove('is-active');
-//       document.querySelector('.nav-list').classList.remove('is-active');
-//     }
-//   }
-
-//   nav.addEventListener('click', closeMenu);
+  navLinks.addEventListener('click', closeMenu);
