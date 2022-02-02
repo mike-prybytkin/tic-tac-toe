42ac11f9d51fc2be4db1cf107c30d27792c464bf
Mike Prybytkin <prybytkin@bk.ru>
2022-02-02 19:32:13 +0300

    feat: add switch photo function

--- nature-sounds/scripts/script.js
+++ nature-sounds/scripts/script.js
@@ -1,3 +1,15 @@
+//switch main image
+
 const btnsBird = document.querySelector(".btns-bird");
 const btnBird = document.querySelectorAll(".btn-bird");
-const btnsBird = document.querySelector(".main-image");
+let mainImage = document.querySelector(".main-image");
+
+function changeImage(event) {
+  mainImage.style.backgroundImage = `none`;
+  let dataAttribute = event.target.dataset.bird;
+  if (event.target.classList.contains("btn-bird")) {
+    mainImage.style.backgroundImage = `url("./assets/img/${dataAttribute}.jpg")`;
+  }
+}
+
+btnsBird.addEventListener("click", changeImage);
