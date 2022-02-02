65e45a6d7f71e448c7b0c6e6026dc8b0e7fceb77
Mike Prybytkin <prybytkin@bk.ru>
2022-02-02 23:14:02 +0300

    feat: add audio player

--- nature-sounds/scripts/script.js
+++ nature-sounds/scripts/script.js
@@ -1,15 +1,50 @@
-//switch main image
+// --- control player ---
 
-const btnsBird = document.querySelector(".btns-bird");
-const btnBird = document.querySelectorAll(".btn-bird");
-let mainImage = document.querySelector(".main-image");
+const audio = new Audio();
+let recordingFile = `./assets/audio/solovey.mp3`; // default bird sound
+const audioLogo = document.querySelector(".audio-logo");
 
-function changeImage(event) {
-  mainImage.style.backgroundImage = `none`;
-  let dataAttribute = event.target.dataset.bird;
-  if (event.target.classList.contains("btn-bird")) {
-    mainImage.style.backgroundImage = `url("./assets/img/${dataAttribute}.jpg")`;
+function controlLogoPlayer() {
+  //switch logo and add class "pause"
+  audioLogo.classList.toggle("pause");
+}
+
+function controlPlayer() {
+  //player controls
+
+  if (audioLogo.classList.contains("pause")) {
+    audio.src = recordingFile;
+    audio.currentTime = 0;
+    audio.play();
+  } else {
+    audio.pause();
   }
 }
 
-btnsBird.addEventListener("click", changeImage);
+audioLogo.addEventListener("click", controlLogoPlayer);
+audioLogo.addEventListener("click", controlPlayer);
+
+//--- switch another bird ---
+
+const btnsBird = document.querySelector(".btns-bird");
+const btnBird = document.querySelectorAll(".btn-bird");
+const mainImage = document.querySelector(".main-image");
+
+function changeBird(event) {
+  let dataAttribute = event.target.dataset.bird;
+  if (event.target.classList.contains("btn-bird")) {
+    mainImage.style.backgroundImage = `url("./assets/img/${dataAttribute}.jpg")`; //URL image
+    recordingFile = `./assets/audio/${dataAttribute}.mp3`; // URL bird sound
+    audioLogo.classList.remove("pause");
+    audio.pause();
+  }
+}
+
+btnsBird.addEventListener("click", changeBird);
+
+//--switch buttons colour--
+
+btnsBird.addEventListener("click", (event) => {
+  btnBird.forEach((el) => el.classList.remove("active"));
+  event.target.classList.add("active");
+});
