b82404fb5da4a84eb0b5ae76a4030e0f7b8c8a87
Mike Prybytkin <prybytkin@bk.ru>
2022-02-10 23:27:43 +0300

    feat: add multilingualism

--- random-jokes/script/script.js
+++ random-jokes/script/script.js
@@ -1,26 +1,51 @@
 const url = "https://type.fit/api/quotes";
-const engLangButton = document.querySelector(".eng-lang");
-const belLangButton = document.querySelector(".bel-lang");
+const engLang = document.querySelector(".eng-lang");
+const belLang = document.querySelector(".bel-lang");
+const languages = document.querySelector(".languages");
+const langButt = document.querySelectorAll(".lang-butt");
 const switchJokeButton = document.querySelector(".switch-joke");
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
+const mainImg = document.querySelector(".main-img");
 
-// async function getData() {
-//   const res = await fetch(url);
-//   const data = await res.json();
+async function getData() {
+  const res = await fetch(url);
+  const data = await res.json();
 
-//   showDataEng(data);
-// }
+  showDataEng(data);
+}
 
-// function showDataEng(data) {
-//   let randomNum = Math.ceil(Math.random() * data.length);
-//   console.log(randomNum);
-//   quote.textContent = data[randomNum].text;
-//   author.textContent = data[randomNum].author;
-// }
+function showDataEng(data) {
+  if (engLang.classList.contains("active-lang")) {
+    let randomNumEng = Math.ceil(Math.random() * data.length);
+    quote.textContent = data[randomNumEng].text;
+    author.textContent = data[randomNumEng].author;
+  }
+}
 
-// getData(); // first launch after opening the page
-// switchJokeButton.addEventListener("click", getData);
+getData(); // first launch after opening the page
+switchJokeButton.addEventListener("click", getData);
+engLang.addEventListener("click", getData);
+
+// switch page lang
+
+languages.addEventListener("click", (event) => {
+  langButt.forEach((el) => el.classList.remove("active-lang"));
+  event.target.classList.add("active-lang");
+});
+
+const engButtText = "say something smart";
+const belButtText = "скажы што разумнае";
+function Translate(lang) {
+  if (lang === "bel") {
+    switchJokeButton.textContent = belButtText;
+  } else {
+    switchJokeButton.textContent = engButtText;
+  }
+}
+
+engLang.addEventListener("click", () => Translate("eng"));
+belLang.addEventListener("click", () => Translate("bel"));
 
 // belarus JSON
 
@@ -31,11 +56,25 @@ async function getQuotes() {
   showDataBel(data);
 }
 function showDataBel(data) {
-  let randomNum = Math.ceil(Math.random() * data.length);
-  console.log(randomNum);
-  quote.textContent = data[randomNum].text;
-  author.textContent = data[randomNum].author;
+  if (belLang.classList.contains("active-lang")) {
+    let randomNumBel = Math.ceil(Math.random() * data.length);
+    quote.textContent = data[randomNumBel].text;
+    author.textContent = data[randomNumBel].author;
+  }
 }
 
 getQuotes();
 switchJokeButton.addEventListener("click", getQuotes);
+belLang.addEventListener("click", getQuotes);
+
+// switch image
+
+function switchImage() {
+  let randomNumImg = Math.ceil(Math.random() * 17);
+  // 17 - number of pictures in a folder "img"
+  mainImg.style.backgroundImage = `url("./assets/img/${randomNumImg}.png")`; //URL image
+}
+switchImage();
+switchJokeButton.addEventListener("click", switchImage);
+engLang.addEventListener("click", switchImage);
+belLang.addEventListener("click", switchImage);
